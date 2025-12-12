77665653a486fb376082afed65004fe1cb847d89
Your Name <you@example.com>
2025-12-12 12:48:57 +0800

    adding endpoints for authentaction

--- src/main/java/com/secj3303/controller/AuthController.java
+++ src/main/java/com/secj3303/controller/AuthController.java
@@ -1,1 +1,40 @@
 package com.secj3303.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    
+    // --- LOGIN ---
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login"; // Routes to login.html
+    }
+
+    @PostMapping("/login")
+    public String processLogin() {
+        return "";
+    }
+
+    // --- LOGOUT ---
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
+    }
+
+    // --- REGISTER ---
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister() {
+        return "redirect:/login";
+    }
+}
