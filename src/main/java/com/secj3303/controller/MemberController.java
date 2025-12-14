36d3e57085126d1958055aecc901a6815f55bbf3
Abdalla-Ali3 <3bda3llah3@gmail.com>
2025-12-14 08:38:15 +0800

    m

--- src/main/java/com/secj3303/controller/MemberController.java
+++ src/main/java/com/secj3303/controller/MemberController.java
@@ -1,27 +1,49 @@
 package com.secj3303.controller;
 
+import com.secj3303.dao.*;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.secj3303.model.Person;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+    @Autowired  
+    private PersonDao personDao;
 
     @GetMapping("/dashboard")
-    public String showDashboard() {
+    public String showDashboard(HttpSession session) {
+        String role = (String) session.getAttribute("role");
+        if (role == null || !"MEMBER".equals(role)) {
+            return "redirect:/login";
+        }
         return "member-dashboard";
     }
 
     @GetMapping("/bmi")
-    public String showBmiForm() {
+    public String showBmiForm(HttpSession session) {
+        String role = (String) session.getAttribute("role");
+        if (role == null || !"MEMBER".equals(role)) {
+            return "redirect:/login";
+        }
         return "member-bmi-form";
     }
 
     @PostMapping("/bmi")
-    public String calculateBmi() {
+    public String calculateBmi(@RequestParam String name, @RequestParam Integer yob, @RequestParam Double height, @RequestParam Double weight, @RequestParam(required = false) String[] interests) {
+
+        Person person = new Person(name, yob, height, weight, interests);
+        
+        personDao.insert(person);
+        
         return "redirect:/member/dashboard";
     }
 
